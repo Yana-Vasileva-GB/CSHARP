6a69b5ab6ec5317ee07af59061625747bfa0d3fb
Yana <jana-ivanova@mail.ru>
2022-09-08 14:56:17 +0400

    Example005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") 
+// ToLower() - перевод символов в нижний регистр
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}!");
+}
